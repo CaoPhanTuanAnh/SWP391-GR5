224d5005212143586646a2510972c515888ed281
totitatu <giaphotboycp123@gmail.com>
2025-02-19 00:53:54 +0700

    Fix loi js change password

--- Cinema/web/assets/js/type-validator.js
+++ Cinema/web/assets/js/type-validator.js
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 let submit = document.querySelector("#form-submit");
+let password = document.querySelector("#password");
+let rePassword = document.querySelector("#re-password");
 submit.disabled = true;
 submit.style = "background-color: grey; border-color:black;";
 let regexPassword1 = /^[A-Za-z\d@.#$!%*?&]{8,16}$/;
@@ -11,37 +13,30 @@ let regexPassword3 = /(?=.*[A-Z])/;
 let regexPassword4 = /(?=.*\d)/;
 let regexPassword5 = /(?=.*[@.#$!%*?&])/;
 let correctPassword = false;
-let correctRePassword = false;
 
-function checkPassword(passwordInput) {
-    let password = passwordInput.value;
-    let mess = passwordInput.nextElementSibling;
+function checkPassword() {
+    let mess = password.nextElementSibling;
     correctPassword = false;
-    if (!regexPassword1.test(password)) {
+    if (!regexPassword1.test(password.value)) {
         mess.innerHTML = "Password must have 8-16 characters!";
-    } else if (!regexPassword2.test(password)) {
+    } else if (!regexPassword2.test(password.value)) {
         mess.innerHTML = "Password must have at least 1 lowercase charaters!";
-    } else if (!regexPassword3.test(password)) {
+    } else if (!regexPassword3.test(password.value)) {
         mess.innerHTML = "Password must have at least 1 uppercase charaters!";
-    } else if (!regexPassword4.test(password)) {
+    } else if (!regexPassword4.test(password.value)) {
         mess.innerHTML = "Password must have at least 1 digit!";
-    } else if (!regexPassword5.test(password)) {
+    } else if (!regexPassword5.test(password.value)) {
         mess.innerHTML = "Password must have at least 1 special charaters!";
     } else {
         mess.innerHTML = "";
         correctPassword = true;
-        allowSubmit();
     }
 }
 
-function checkRePassword(rePasswordInput) {
-    let password = document.querySelector("#password").value;
-    let rePassword = rePasswordInput.value;
-    let mess = rePasswordInput.nextElementSibling;
-    correctRePassword = false;
-    if (password === rePassword) {
+function checkRePassword() {
+    let mess = rePassword.nextElementSibling;
+    if (password.value === rePassword.value) {
         mess.innerHTML = "";
-        correctRePassword = true;
         allowSubmit();
     } else {
         mess.innerHTML = "Re-entered password is incorrect!";
@@ -49,7 +44,7 @@ function checkRePassword(rePasswordInput) {
 }
 
 function allowSubmit(){
-    if(correctPassword&&correctRePassword){
+    if(correctPassword){
         submit.disabled = false;
         submit.style = "";
     }
