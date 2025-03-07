b4ce100f208f5c429486e4bf59a12979c090931f
Mynameiskiet <104210040+Mynameiskiet@users.noreply.github.com>
2025-03-07 10:40:29 +0700

    update

--- Cinema/src/java/entity/genres.java
+++ Cinema/src/java/entity/genres.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entity;
 
 import lombok.AllArgsConstructor;
@@ -11,10 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-/**
- *
- * @author PCASUS
- */
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
